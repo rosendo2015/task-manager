46923356d86cc0bd9fd1cf4afd0188e8bf21a6c1
rosendo <rosendc30@gmail.com>
2026-03-25 00:20:07 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { routes } from "./routes";
+import { errorHandling } from "./middleware/error-handling";
+
+const app = express()
+app.use(express.json())
+app.use(routes)
+app.use(errorHandling)
+
+export { app }
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { Request, Response } from "express"
+import { hash } from "bcrypt"
+import { z } from "zod"
+
+class UsersController {
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            name: z.string().min(6),
+            email: z.email(),
+            password: z.string().min(6),
+            role: z.enum(["admin", "member"])
+        })
+        const { name, email, password, role } = bodySchema.parse(request.body)
+        const userWithSameEmail = await prisma.users.findUnique({ where: { email } })
+        if (userWithSameEmail) {
+            throw new AppError("Email already exists", 400)
+        }
+        const hashedPassword = await hash(password, 8)
+        const user = await prisma.users.create({
+            data: {
+                name, email, password: hashedPassword, role: "member"
+            }
+        })
+        const { password: _, ...userWithoutPassword } = user
+        return response.json(userWithoutPassword)
+    }
+}
+export { UsersController }
